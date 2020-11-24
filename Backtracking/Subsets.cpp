4dd5b2b9a17756b61f4aa54fc8e26dc58a35254e
rogueprogrammer <dkhanna@uwaterloo.ca>
2020-11-23 19:08:55 -0800

    Update Subsets.cpp

--- Backtracking/Subsets.cpp
+++ Backtracking/Subsets.cpp
@@ -1,13 +1,15 @@
 // https://leetcode.com/problems/subsets/solution/
 class Solution {
 public:
-    /*  s = {1, 2, 3} 
-* recursion tree below, at each node, we decide whether to include or exclude the number at the current level.
+    /* if we consider set s = {1, 2, 3} 
+we can create recursion tree below of all subsets, at each node, we decide whether to include or exclude the number at the current level. final result is all the leaf nodes
                      {}                                                       
         {}                            {1}                                     i = 0, do we include s[0] = 1, left is exclude 1, right is to include 1
     {}    {2}                 {1}            {1,2}                            i = 1, do we include s[1] = 2, left is to exclude 2, right is to include 2
  {}   {3}   {2}  {2, 3}    {1}  {1,2}   {1,2}     {1,2,3}                     i = 2, do we include s[2] = 3, left is to exclude 3, right is to include 3
 
+Runtime: O(((2^n)/2)*n) = O((2^n)*n) because the number of leaf nodes = 2^n, and each node needs to copy the set of numbers into the result vector, which is an O(n) runtime
+operation.
 */     
     vector<vector<int>> res;
     
