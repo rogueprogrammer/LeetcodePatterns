90dd0fd969d4e27caad17a2f1e99c0c7fde0797c
rogueprogrammer <dkhanna@uwaterloo.ca>
2020-11-23 17:29:27 -0800

    Update LetterCasePermutation.cpp

--- Backtracking/LetterCasePermutation.cpp
+++ Backtracking/LetterCasePermutation.cpp
@@ -11,18 +11,23 @@ public:
 a1b2  a1B2    A1b2    A1B2      i = 2, two choices, b and B
 a1b2  a1B2    A1b2    A1B2      i = 3, no choices, go to next index
                                 i = 4, i >= s.size => return leaf node (which is just s)
+                                
+    space complexity = 2^n nodes in above tree, each node is a string of length n => O((2^n)*n)
+    time complexity = O((2^n)/2  *n) <- time taken for (2^n)/2 internal nodes
+                           
     */
     
     
     // s is the object to modify, index is the index within s to modify
     void helper(string s, int index){
         
-        //base case
+        //base case, leaf node
         if(index >= s.size()){
             res.push_back(s);
             return;
         }
         
+        // internal nodes:
         if(isalpha(s[index])){
             if(islower(s[index])){
                 helper(s, index+1);
